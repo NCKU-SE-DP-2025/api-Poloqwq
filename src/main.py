a917d093d15c1e0aa12abd88d113778747b239fb
Poloqwq <fuxang.yai@gmail.com>
2025-10-15 14:18:01 +0800

    1. adding router to APIs having same URL prefix\n 2. edit the URL of rectangle-area to avoid potential routing error

--- src/main.py
+++ src/main.py
@@ -1,23 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
 
 app = FastAPI()
+router = APIRouter(prefix="/api/v1")
 
 class Words(BaseModel):
     words: list[str]
 
-@app.get("/api/v1/me")
+@router.get("/me")
 def read_me():
     return {"name": "葉富祥", "student_id": "F74146864"}
 
-@app.get("/api/v1/{purpose}")
-def calc_area(purpose: str, width: int, height: int):
+@router.get("/rectangle-area")
+def calc_area(width: int, height: int):
     area = width * height
     return area
 
-@app.post("/api/v1/word-length-calculator")
+@router.post("/word-length-calculator")
 def word_length_calculator(words: Words):
     response = []
     for word in words.words:
         response.append({"word": word, "length": len(word)})
     return response
+
+app.include_router(router)
